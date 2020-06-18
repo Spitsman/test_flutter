16c2315e686a76f7ef517bf65cb5a088ad03aead
Alexander Spitsyn <sanspits@yandex.ru>
2020-06-18 18:59:34 +0300

    lesson 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,118 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.red,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page 123'),
-    );
-  }
-}
+class NewsBoxFavorite extends StatefulWidget {
+ final int _num;
+ final bool _like;
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+ NewsBoxFavorite(this._num, this._like);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  createState() => new NewsBoxFavoriteState(_num, _like);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class NewsBoxFavoriteState extends State<NewsBoxFavorite> {
+  int num;
+  bool like = false;
 
-  void _incrementCounter() {
+  NewsBoxFavoriteState(this.num, this.like);
+
+  void pressButton() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      like = !like;
+
+      like ? num++ : num--;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return new Column(children: <Widget>[
+      new Text('В избранном\n$num', textAlign: TextAlign.center),
+      new IconButton(
+        icon: new Icon(like ? Icons.star : Icons.star_border, size: 30.0, color: Colors.blue[500]),
+        onPressed: pressButton
+      )
+    ],);
+  }
+}
+
+class NewsBox extends StatelessWidget {
+  final String _title;
+  final String _text;
+  String _imageurl;
+  int _num;
+  bool _like;
+
+  NewsBox(this._title, this._text, {String imageurl, int num = 0, bool like = false}) {
+    _imageurl = imageurl;
+    _num = num;
+    _like = like;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black12,
+      height: 100.0,
+      child: new Row(children: <Widget>[
+        new Image.network(_imageurl, width: 100.0, height: 100.0, fit: BoxFit.cover),
+        new Expanded(child:
+          new Container(
+            padding: EdgeInsets.all(5.0),
+            child: new Column(
+              children: <Widget>[
+                new Text(_title, style: new TextStyle(fontSize: 20.0)),
+                new Text(_text, softWrap: true, textAlign: TextAlign.justify,)
+              ],
+            )
+          )
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        new NewsBoxFavorite(_num, _like)
+      ],)
+    );
+  }
+}
+
+void main() {
+  runApp(
+    new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new Scaffold(
+        appBar: new AppBar(title: new Text('Biba')),
+        body: new NewsBox(
+          'Lorem Ipsum',
+          'Quia officia rerum. Quasi ducimus et. Repellat cum autem.',
+          imageurl: 'https://297228.selcdn.ru/backend/uploads/profile/avatar/580/%D1%88%D0%B0%D1%83%D1%80%D0%BC%D0%B0.webp',
+          like: true,
+          num: 101
+        )
+      )
+    )
+  );
+}
+
+
+
+// Stateless
+
+class MyBody extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return new Center(
+        child: new Column(
+          children: <Widget>[
+            new Text('hello'),
+            new FlatButton(
+              onPressed: () async {
+                const url = 'https://google.com';
+
+                if (await canLaunch(url)) {
+                  await launch(url);
+                } else {
+                  throw 'Error $url';
+                }
+              },
+              child: new Text('qwert'),
+              color: Colors.amber,
+              textColor: Colors.white,
+            )
+          ],
+        )
     );
   }
 }
